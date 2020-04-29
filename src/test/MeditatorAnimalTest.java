4b8d4b93ec8c4b8405d8a81ae26cda349728ed5f
Anne Landrum <aelandru@asu.edu>
2020-04-28 18:51:13 -0700

    Added first Junit test

---
+++ src/test/MeditatorAnimalTest.java
@@ -0,0 +1,52 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import Decorator.Animals;
+
+public class MeditatorAnimalTest {
+
+    // A sample Cart
+
+    Animals anim1;
+    int anim1Expected;
+    
+    Animals anim2;
+    int anim2Expected;
+    
+    Animals anim3;
+    int anim3Expected;
+
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        anim1 = new Animals();
+        anim1Expected = 1;
+        
+        anim2 = new Animals();
+        anim2Expected = 1;
+        
+        anim3 = new Animals();
+        anim3Expected = 2;
+    }
+
+    // anim1
+    @Test
+    public void anim1() {
+        double amount = anim1.decorate();
+        assertEquals(anim1Expected, amount, 5);
+    }
+    
+    // anim2
+    @Test
+    public void anim3() {
+        double amount = anim2.getAnimalAffinity();
+        assertEquals(anim2Expected, amount, 5);
+    }
+    
+    // anim2
+    @Test
+    public void anim4() {
+        double amount = anim3.getNumAnimals();
+        assertEquals(anim3Expected, amount, 0);
+    }
+}
